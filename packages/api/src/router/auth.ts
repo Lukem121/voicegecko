bec0069fb0225b2d17bf06dd1a1655893c3d70e2
Luke Askew <lukeask@hotmail.co.uk>
2025-06-30 03:13:24 +0100

    Integrate tauri-plugin-opener and update related configurations. Removed tauri-plugin-single-instance, added new plugin dependencies, and adjusted package.json and Cargo.toml files. Enhanced authentication flow with new secret message mutation and improved UI for sign-in and sign-out actions.

--- packages/api/src/router/auth.ts
+++ packages/api/src/router/auth.ts
@@ -1,4 +1,5 @@
 import type { TRPCRouterRecord } from "@trpc/server";
+import { z } from "zod/v4";
 
 import { protectedProcedure, publicProcedure } from "../trpc";
 
@@ -6,7 +7,13 @@ export const authRouter = {
   getSession: publicProcedure.query(({ ctx }) => {
     return ctx.session;
   }),
-  getSecretMessage: protectedProcedure.query(() => {
-    return "you can see this secret message!";
-  }),
+  getSecretMessage: protectedProcedure
+    .input(
+      z.object({
+        message: z.string(),
+      }),
+    )
+    .mutation(({ input }) => {
+      return "you can see this secret message!";
+    }),
 } satisfies TRPCRouterRecord;
