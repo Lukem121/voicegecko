83ac147172e84cbdbaad445e2b7080031214380b
Luke Askew <lukeask@hotmail.co.uk>
2025-07-03 22:30:06 +0100

    Refactor Tailwind configuration and remove AuthShowcase component
    - Updated Tailwind configuration to use 'tailwind' import for font families and adjusted presets type.
    - Removed the AuthShowcase component to streamline authentication handling and reduce code complexity.
    - Modified dark mode settings in base Tailwind configuration to include 'system' option for enhanced user experience.

--- apps/desktop/tailwind.config.ts
+++ apps/desktop/tailwind.config.ts
@@ -1,5 +1,5 @@
 import type { Config } from "tailwindcss";
-import { fontFamily } from "tailwindcss/defaultTheme";
+import tailwind from "tailwindcss/defaultTheme";
 
 import baseConfig from "@acme/tailwind-config/web";
 
@@ -7,12 +7,12 @@ export default {
   // We need to append the path to the UI package to the content array so that
   // those classes are included correctly.
   content: [...baseConfig.content, "../../packages/ui/**/*.{ts,tsx}"],
-  presets: [baseConfig],
+  presets: [baseConfig as any],
   theme: {
     extend: {
       fontFamily: {
-        sans: ["var(--font-sans)", ...fontFamily.sans],
-        mono: [...fontFamily.mono],
+        sans: ["var(--font-sans)", ...tailwind.fontFamily.sans],
+        mono: [...tailwind.fontFamily.mono],
       },
     },
   },
